310ae50a5b053c445d05a1308d6f355fbae6db5f
Xannara <roksanasky@gmail.com>
2020-10-13 02:23:04 +0300

    Получение текущего времени локально

--- src/main/java/GetCurrentTime.java
+++ src/main/java/GetCurrentTime.java
@@ -12,13 +12,13 @@ public class GetCurrentTime {
 
     public static void printTime(String username, int currentTime) {
 
-        if(currentTime >= 0 && currentTime < 4 ) {
+        if(currentTime >= 0 && currentTime <= 4 ) {
             System.out.println("Доброй ночи, " + username);
-        } else if (currentTime >=5 && currentTime < 9) {
+        } else if (currentTime >=5 && currentTime <= 9) {
             System.out.println("Доброе утро" + username);
-        } else if (currentTime >= 10 && currentTime < 16) {
+        } else if (currentTime >= 10 && currentTime <= 16) {
             System.out.println("Добрый день" + username);
-        } else if (currentTime >= 17 && currentTime < 23) {
+        } else if (currentTime >= 17 && currentTime <= 23) {
             System.out.println("Добрый вечер" + username);
         } else {
             System.out.println("Время не попадает в указанные диапазоны");
